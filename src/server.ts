beb8f4f88684f12c68f5dfceae4e88de56805117
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-11-15 11:59:23 -0300

    feat: Adds and configures CORS in the application This commit introduces the `cors` middleware to allow requests from different origins (Cross-Origin Resource Sharing). The `cors` library was installed as a dependency and configured in the `server.ts` file to accept requests from any origin, facilitating communication with front-end applications running on different domains.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import journeyRoutes from "./routes/journeyRoutes";
+import cors from "cors";
 
 const PORT = 3333;
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
